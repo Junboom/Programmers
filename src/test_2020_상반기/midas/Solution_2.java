3e3ba97c825d7749b17bde9a7e1af3ef5ba0fcc0
junboom <ticktock0414@gmail.com>
2020-05-23 21:19:38 +0900

    midas test

---
+++ src/test_2020_상반기/midas/Solution_2.java
@@ -0,0 +1,28 @@
+package test_2020_상반기.midas;
+
+public class Solution_2 {
+
+	public static int solution(int x, int y, int r, int d, int[][] target) {
+		int answer = 0;
+		double click = Math.toDegrees(Math.atan2(y, x));
+		
+		for (int i = 0; i < target.length; i++) {
+			int tx = target[i][0];
+			int ty = target[i][1];
+
+			double tangle = Math.toDegrees(Math.atan2(ty, tx));
+			if (tangle > click + d || tangle < click - d || Math.sqrt(ty * ty + tx * tx) > r)
+				continue;
+			
+			++answer;
+		}
+		
+		return answer;
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println(solution(-1, 2, 2, 60, new int[][] { {0, 1}, {-1, 1}, {1, 0}, {-2, 2} }));
+	}
+
+}
---
+++ src/test_2020_상반기/midas/Solution_3.java
@@ -0,0 +1,91 @@
+package test_2020_상반기.midas;
+
+public class Solution_3 {
+
+	public static int answer, max, cLen, rLen;
+	public static int[][] copy;
+	public static boolean[][] v;
+	
+	public static void hit(int i, int j) {
+		max = 1;
+		copy[i][j] = 0;
+		while (0 < i) {
+			copy[i][j] = copy[--i][j];
+			copy[i][j] = 0;
+		}
+		
+		int cnt;
+		do {
+			cnt = check(0);
+			crush();
+			down();
+			
+		} while (0 < cnt);
+	}
+	
+	public static int check(int cnt) {
+		v = new boolean[cLen][rLen];
+		
+		for (int i = 0; i < cLen; ++i) {
+			for (int j = 0; j < rLen; ++j) {
+				if (copy[i][j] == 0)
+					continue;
+				
+				if (j < rLen - 2 && copy[i][j] == copy[i][j + 1] && copy[i][j] == copy[i][j + 2]) {
+					v[i][j] = true;
+					v[i][j + 1] = true;
+					v[i][j + 2] = true;
+					++cnt;
+				}
+				if (i < cLen - 2 && copy[i][j] == copy[i + 1][j] && copy[i][j] == copy[i + 2][j]) {
+					v[i][j] = true;
+					v[i + 1][j] = true;
+					v[i + 2][j] = true;
+					++cnt;
+				}
+			}
+		}
+		
+		return cnt;
+	}
+	
+	public static void crush() {
+		for (int i = 0; i < cLen; ++i) {
+			for (int j = 0; j < rLen; ++j) {
+				if (v[i][j] && copy[i][j] != 0) {
+					copy[i][j] = 0;
+					++max;
+				}
+			}
+		}
+	}
+	
+	public static void down() {
+		
+	}
+	
+	public static int solution(int[][] board) {
+		answer = 0;
+		cLen = board.length;
+		rLen = board[0].length;
+		
+		for (int i = 0; i < cLen; ++i) {
+			for (int j = 0; j < rLen; ++j) {
+				copy = new int[cLen][rLen];
+				for (int k = 0; k < cLen; ++k)
+					copy[k] = board[k].clone();
+				
+				hit(i, j);
+				
+				answer = answer < max ? max : answer;
+			}
+		}
+		
+		return answer;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(solution(new int[][] { {1, 1, 3, 3}, {4, 1, 3, 4}, {1, 2 ,1 ,1}, {2, 1, 3, 2} }));
+	}
+
+}
