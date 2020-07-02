0ce5c0699bc71eec5552517e1e4fa3a4b11498ba
junboom <ticktock0414@gmail.com>
2020-07-03 01:46:11 +0900

    update

--- src/skill/check/test/level3/Solution_디스크컨트롤러.java
+++ src/skill/check/test/level3/Solution_디스크컨트롤러.java
@@ -7,22 +7,22 @@ public class Solution_디스크컨트롤러 {
 	public static int solution(int[][] jobs) {
 		int answer = 0;
 		int len = jobs.length;
-		int end = 0;
+		int time = 0;
 		int idx = 0;
 		Queue<int[]> q = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
 		
 		Arrays.sort(jobs, (o1, o2) -> o1[0] - o2[0]);
 		
 		while (idx < len || !q.isEmpty()) {
-			while (idx < len && jobs[idx][0] <= end)
+			while (idx < len && jobs[idx][0] <= time)
 				q.offer(jobs[idx++]);
 			
 			if (q.isEmpty())
-				end = jobs[idx][0];
+				time = jobs[idx][0];
 			else {
 				int[] job = q.poll();
-				answer += end - job[0] + job[1];
-				end += job[1];
+				answer += time - job[0] + job[1];
+				time += job[1];
 			}
 		}
 		
